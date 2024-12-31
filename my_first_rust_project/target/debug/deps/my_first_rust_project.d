b31529fb08433f3841da80a4f0a907d2d71ca942
emotionslos <paulloibl1@gmail.com>
2024-12-31 20:56:53 +0100

    first commit

---
+++ my_first_rust_project/target/debug/deps/my_first_rust_project.d
@@ -0,0 +1,5 @@
+C:\Users\paull\Desktop\me\Progammiersprachen\Rust\my_first_rust_project\target\debug\deps\my_first_rust_project.exe: src/main.rs
+
+C:\Users\paull\Desktop\me\Progammiersprachen\Rust\my_first_rust_project\target\debug\deps\my_first_rust_project.d: src/main.rs
+
+src/main.rs:
---
+++ my_first_rust_project/target/debug/my_first_rust_project.d
@@ -0,0 +1,1 @@
+C:\Users\paull\Desktop\me\Progammiersprachen\Rust\my_first_rust_project\target\debug\my_first_rust_project.exe: C:\Users\paull\Desktop\me\Progammiersprachen\Rust\my_first_rust_project\src\main.rs
