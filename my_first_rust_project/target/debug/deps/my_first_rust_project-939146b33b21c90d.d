8bf7645d4559e6348cff7b73889df0c88cefdd49
emotionslos <paulloibl1@gmail.com>
2025-01-01 01:45:32 +0100

    second commit

---
+++ my_first_rust_project/target/debug/deps/my_first_rust_project-939146b33b21c90d.d
@@ -0,0 +1,5 @@
+C:\Users\paull\Desktop\me\Progammiersprachen\Rust\my_first_rust_project\target\debug\deps\libmy_first_rust_project-939146b33b21c90d.rmeta: src/main.rs
+
+C:\Users\paull\Desktop\me\Progammiersprachen\Rust\my_first_rust_project\target\debug\deps\my_first_rust_project-939146b33b21c90d.d: src/main.rs
+
+src/main.rs:
---
+++ my_first_rust_project/target/debug/deps/my_first_rust_project-a5873f668f1736af.d
@@ -0,0 +1,5 @@
+C:\Users\paull\Desktop\me\Progammiersprachen\Rust\my_first_rust_project\target\debug\deps\libmy_first_rust_project-a5873f668f1736af.rmeta: src/main.rs
+
+C:\Users\paull\Desktop\me\Progammiersprachen\Rust\my_first_rust_project\target\debug\deps\my_first_rust_project-a5873f668f1736af.d: src/main.rs
+
+src/main.rs:
